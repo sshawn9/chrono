ebf34c28f373b5beafe0a647ae6307541ddee6bc
tasora <tasora@ied.unipr.it>
2026-04-14 15:41:08 +0200

    Hyperelastic materials provide a default fallback for computing tangent modulus via numerical differentiation if the developer does not provide an override with a fast analytical expression.

--- src/chrono/fea/ChMaterial3DHyperelastic.h
+++ src/chrono/fea/ChMaterial3DHyperelastic.h
@@ -27,7 +27,7 @@ namespace fea {
 
 
 /// Class for hyperelastic materials, that is materials with a constitutive equation 
-/// of the type  P = f(C) , where P is the 2nd Piola-Kirchhoff strain and 
+/// of the type  S = f(C) , where S is the 2nd Piola-Kirchhoff stress and 
 /// C is the right Cauchy-Green deformation tensor C=F^T*F  (in material space)
 /// as in E = 1/2(C -I) where E the Green Lagrange strain tensor. 
 /// Or in general, materials for which the stress–strain 
@@ -76,14 +76,56 @@ public:
         return false;
     }
 
-    /// Compute elastic stress from the right Cauchy-Green deformation tensor C = F^T*F. 
-    /// Return stress as Piola-Kirchhoff S tensor, in Voigt notation. 
+    /// Compute elastic stress from the right Cauchy-Green deformation tensor C_def = F^T*F. 
+    /// Return stress as 2nd Piola-Kirchhoff S tensor, in Voigt notation. 
     /// Children materials MUST implement this.
-    virtual void ComputeElasticStress(ChStressTensor<>& S_stress, const ChMatrix33d& C_deformation) = 0;
+    
+    virtual void ComputeElasticStress(ChStressTensor<>& S_stress, const ChMatrix33d& C_def) = 0;
 
     /// Computes the tangent modulus for a given deformation.
-    /// Children materials MUST implement this.
-    virtual void ComputeElasticTangentModulus(ChMatrixNM<double, 6, 6>& StressStrainMatrix, const ChMatrix33d& C_deformation) = 0;
+    /// It takes the right Cauchy-Green deformation tensor C_def, and returns the tangent 
+    /// modulus in 6x6 matrix C_tangent, for  dS = C_tangent * dE  where S is 2nd Piola-Kirchhoff stress, and E is Green Lagrange strain.
+    /// A fallback implementation is provided here, that computes C via numerical differentiation by calling 6 times
+    /// the ComputeElasticStress() function. However, if you are able to compute a custom analytical expression of C_tangent, 
+    /// and the analytical expression is fast to compute, then you SHOULD override this.
+    
+    virtual void ComputeElasticTangentModulus(ChMatrixNM<double, 6, 6>& C_tangent, const ChMatrix33d& C_def) {
+        // Step size for perturbation (relative)
+        const double eps = 1e-6;
+
+        ChStressTensor S0_voigt;
+        ComputeElasticStress(S0_voigt, C_def);
+
+        C_tangent.setZero();
+
+        ChStrainEngTensor C_voigt;
+        C_voigt.ConvertFromMatrix(C_def);
+
+        // Perturb each component of C
+        for (int i = 0; i < 6; ++i) {
+            // Compute step size
+            double h = eps;
+            if (std::abs(C_voigt(i)) > 1e-8) {
+                h = eps * std::abs(C_voigt(i));  // relative perturbation
+            }
+
+            ChStrainEngTensor C_plus_voigt = C_voigt;
+            C_plus_voigt(i) += h;
+
+            ChMatrix33d C_perturbed;
+            C_plus_voigt.ConvertToMatrix(C_perturbed);
+
+            // Compute stress at perturbed C
+            ChStressTensor S_plus_voigt;
+            ComputeElasticStress(S_plus_voigt, C_perturbed);
+
+            // Forward difference for dS/dC = (S_plus_voigt - S0_voigt) / h
+            // But we need dS/dE. Since E = 0.5*(C - I), we have dE = 0.5*dC. Hence the 2.0 factor here.
+            ChVectorN<double, 6> dS_dE = (S_plus_voigt - S0_voigt) * (2.0 / h);
+
+            C_tangent.col(i) = dS_dE;
+        }
+    }
 };
 
 
